7f0aba717c827e041de8825a87385f099556dd83
Jonathan W <wallerjonathan23@gmail.com>
2025-01-13 22:59:38 -0500

    Added servo testing

--- MotorTest/MotorTest.ino
+++ MotorTest/MotorTest.ino
@@ -1,14 +1,19 @@
 #include <AccelStepper.h>
+#include <Servo.h>
 
 // Declare pins
 const int stepXPin = 2;
 const int dirXPin = 5;
 const int stepYPin = 3;
 const int dirYPin = 6;
+const int servoPin = 4; // stepZPin
 
 // Declare Stepper Motor(s)
 AccelStepper stepperX(AccelStepper::DRIVER, stepXPin, dirXPin);
 AccelStepper stepperY(AccelStepper::DRIVER, stepYPin, dirYPin);
+
+// Declare Servo Motor(s)
+Servo writingServo;
 
 void setup() {
   // Set stepper params
@@ -20,13 +25,16 @@ void setup() {
   stepperY.setSpeed(100.0);
   //stepperY.setAcceleration(1000.0);
   //stepperY.moveTo(1000);
+
+  // Set up servo params
+  writingServo.attach(servoPin);
 }
 
 void loop() {
   // Run tests
-
   stepperX.runSpeed();
   stepperY.runSpeed();
+  writingServo.write(180);
 
   /*
   if(!stepperX.runSpeed()) {
