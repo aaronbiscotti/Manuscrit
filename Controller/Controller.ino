1b25866780e33861e507c28609318daa84d07a72
Jonathan W <wallerjonathan23@gmail.com>
2025-01-15 19:35:15 -0500

    Callibration

--- Controller/Controller.ino
+++ Controller/Controller.ino
@@ -3,11 +3,13 @@
 
 /****************** VARIABLES ******************/
 // Constants (TO BE TWEAKED WHEN BUILT)
-#define WRITE_MODE 1
-#define TRAVEL_MODE 0
+#define WRITE_MODE 0
+#define TRAVEL_MODE 1
+#define CALIBRATE_MODE 2
 #define STEPPER_MAX_SPEED 1000.0 // 1000.0 if no micro steps
 #define STEPPER_WRITE_SPEED 200.0 // 200.0 if no micro steps
 #define STEPPER_TRAVEL_SPEED 600.0 // 600.0 if no micro steps
+#define STEPPER_CALIBRATE_SPEED 100.0 // 100.0 if no micro steps
 #define SERVO_UP 160
 #define SERVO_DOWN 30
 
@@ -22,8 +24,8 @@ const int servoPin = 4; // stepZPin
 char mode;
 
 // Current position variables
-unsigned float currX;
-unsigned float currY;
+float currX;
+float currY;
 
 // Declare Stepper Motor(s)
 AccelStepper stepperX(AccelStepper::DRIVER, stepXPin, dirXPin);
@@ -69,6 +71,39 @@ void travelMode() {
 }
 
 /**
+ * /brief Calibrates the machine by bringing the head to (0, 0)
+ */
+void calibrate() {
+  // Set calibrate mode
+  mode = CALIBRATE_MODE;
+
+  // Set calibrate speed
+  stepperX.setSpeed(STEPPER_CALIBRATE_SPEED);
+  stepperY.setSpeed(STEPPER_CALIBRATE_SPEED);
+
+  // Put into writing position
+  writingServo.write(SERVO_UP);
+
+  // Get x == 0.0 first
+  // While is no connection between for the head and x axis
+  while(false) {
+    stepperX.runSpeed();
+  }
+  stepperX.stop();
+  currX = 0.0;
+
+  // Get y == 0.0 second
+  // While is no connection between the head and y axis
+  while(false) {
+    stepperY.runSpeed();
+  }
+  stepperY.stop();
+  currY = 0.0;
+
+  return;
+}
+
+/**
  * /brief Goes to the (x, y) coordinates given
  */
 void goTo(float x, float y) {
@@ -78,22 +113,23 @@ void goTo(float x, float y) {
 /****************** RUNTIME ******************/
 
 void setup() {
-  // Set the starting mode
-  mode = TRAVEL_MODE;
-
   // Set stepper params
   stepperX.setMaxSpeed(STEPPER_MAX_SPEED);
-  stepperX.setSpeed(STEPPER_TRAVEL_SPEED);
   stepperY.setMaxSpeed(STEPPER_MAX_SPEED);
-  stepperY.setSpeed(STEPPER_TRAVEL_SPEED);
 
   // Set up servo params
   writingServo.attach(servoPin);
+
+  // Calibrate the device on startup
+  calibrate();
+
+  // Set the starting mode
+  travelMode();
 }
 
 void loop() {
   // Run tests
+  travelMode();
   stepperX.runSpeed();
   stepperY.runSpeed();
-  up(writingServo);
 }
